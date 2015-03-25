f68e1cd2406d333b4aa2312dd81ce4fbac881d41
ghostbod99 <ghostbod99@gmail.com>
2015-03-25 15:47:31 +0800

    add method

--- ezmysql.py
+++ ezmysql.py
@@ -58,7 +58,8 @@ class Connection(object):
     """
     def __init__(self, host, port, user, password,
                  database='',
-                 charset='utf8'):
+                 charset='utf8',
+                 autocommit=1):
 
 
         self.host = host
@@ -173,6 +174,24 @@ class Connection(object):
         column_names = [d[0] for d in r.fields]
         return [Row(itertools.izip(column_names, row)) for row in r.rows]
 
+
+    def start_transaction(self):
+        return self.execute("START TRANSACTION")
+
+    def commit(self):
+        return self.execute("COMMIT")
+
+    def rollback(self):
+        return self.execute("ROLLBACK")
+
+
+    def query(self, query, *args):
+        """Returns a row list for the given query and args."""
+        r = self.execute(query, *args)
+        column_names = [d[0] for d in r.fields]
+        return [Row(itertools.izip(column_names, row)) for row in r.rows]
+
+
     def get(self, query, *args):
         """Returns the first row returned for the given query."""
         r = self.execute(query, *args)
@@ -188,6 +207,27 @@ class Connection(object):
         d = self.get(sql)
         if d is not None: return True
         return False
+
+
+    def is_in_table_by_wheres(self, table_name, field, where_dict):
+        
+        wheres = []
+        for k in where_dict.keys():
+            s = '%s=%%s' % k
+            wheres.append(s)
+        wheres = " AND ".join(wheres)
+
+        sql = 'SELECT %s FROM %s WHERE %s' % (field, table_name, wheres)
+        args = where_dict.values()
+
+        print sql, args
+
+        d = self.get(sql, *args)
+        if d is not None: return True
+        return False
+
+
+
 
     def update_table(self, table_name, updates,
                      where_field, where_value):
@@ -206,47 +246,211 @@ class Connection(object):
         args.append(where_value)
         return self.execute(sql, *args)
 
+    select_process = {
+        "__in_": lambda k, where_dict: '%s in (%s)' % ( k, where_dict[k]['__in_'] ),
+        "__ne_": lambda k, where_dict: '%s!=%s' % (k, where_dict[k]['__ne_']),
+        "__lt_": lambda k, where_dict: '%s<%s' % (k, where_dict[k]['__lt_']),
+        "__lte_": lambda k, where_dict: '%s<=%s' % (k, where_dict[k]['__lte_']),
+        "__gt_": lambda k, where_dict: '%s>%s' % (k, where_dict[k]['__gt_']),
+        "__gte_": lambda k, where_dict: '%s>=%s' % (k, where_dict[k]['__gte_']),
+        "__like_": lambda k, where_dict: "%s LIKE '%s%%%%'" % (k, where_dict[k]['__like_']),
+        "__all_like_": lambda k, where_dict: "%s LIKE '%%%%%s%%%%'" % (k, where_dict[k]['__all_like_']),
+    }
 
-    def select_table_by_fields(self, table_name, select_fields,
-                     where_dict, limit_conf=None, select_type="list"):
-        selects = []
-        for k in select_fields:
-            s = '%s' % k
-            selects.append(s)
-        selects = ','.join(selects)
+    def select_table_by_wheres(self, table_name, select_fields, where_dict, limit_conf=None, select_type="list", group_by_fields=None, order_by_fields=None, lock=False):
+        '''根据条件查询记录  add by ghostbod'''
 
+
+        selects = ','.join(select_fields)
 
         wheres = []
         for k in where_dict.keys():
-            s = '%s=%%s' % k
+            if type(where_dict[k])==dict:
+                protype = where_dict[k].keys()[0]
+                if protype in self.select_process.keys():
+                    s = self.select_process[protype](k, where_dict)
+                where_dict.pop(k)
+            else:
+                s = '%s=%%s' % k
             wheres.append(s)
-        wheres = " AND ".join(wheres)
 
-        sql = 'SELECT %s FROM %s WHERE %s' % (
-            selects,
-            table_name,
-            wheres
-        )
+        if len(wheres) != 0:
+            wheres = " AND ".join(wheres)
+
+        if len(wheres)==0:
+            sql = 'SELECT %s FROM %s ' % (selects, table_name)
+        else:
+            sql = 'SELECT %s FROM %s WHERE %s' % (selects, table_name, wheres)
+
+        if group_by_fields is not None:
+            group_bys = ','.join(group_by_fields)
+            sql += ' GROUP BY %s' % group_bys
+
+        if order_by_fields is not None:
+            order_bys = ','.join(order_by_fields)
+            sql += ' ORDER BY %s' % order_bys
 
         if limit_conf is not None:
-            select_type = "list"
+            # select_type = "list"
             sql += ' LIMIT %s, %s' % (limit_conf['start'], limit_conf['count'])
 
         args = where_dict.values()
+        lock_str = " FOR UPDATE " if lock == True else ""
         
-        print sql, args
+        if select_type=="get":
+            print sql+" LIMIT 1"+lock_str, args
+            return self.get(sql+" LIMIT 1"+lock_str, *args)
+        else:
+            print sql+lock_str, args
+            return self.query(sql+lock_str, *args)
+
+
+
+    def select_tables_by_wheres(self, table, join_tables, select_fields, where_dict, limit_conf=None, select_type="list", group_by_fields=None, order_by_fields=None, lock=False):
+        '''
+            根据条件查询记录  add by ghostbod
+
+            select_fields =['*']
+            table = {"name": "table1", "alias": "t1"}
+            join_tables=[{
+                "name": "table2",
+                "alias": "t2",
+                "on": ["t1.id=t2.id", "t1.cid=t2.cid"]
+            },{
+                "name": "table3",
+                "alias": "t3",
+                "on": ["t3.id=t2.id", "t3.cid=t2.cid"]
+            }]
+
+            where_dict = {
+                "t1.id": 1,
+                "t2.id": 2
+            }
+        '''
+
+        # print table, join_tables, select_fields, where_dict, limit_conf, select_type, group_by_fields, order_by_fields, lock
+        selects = ','.join(select_fields)
+        joins = []
+        for k in join_tables:
+            s = 'LEFT JOIN %s %s ON %s' % (k['name'], k['alias'], ' AND '.join(k['on']) )
+            joins.append(s)
+        joins = ' '.join(joins)
+        print where_dict
+        print where_dict.keys()
+        wheres = []
+        for k in where_dict.keys():
+            if type(where_dict[k])==dict:
+                print k, where_dict[k].keys()
+                protype = where_dict[k].keys()[0]
+                if protype in self.select_process.keys():
+                    s = self.select_process[protype](k, where_dict)
+                where_dict.pop(k)
+            else:
+                s = '%s=%%s' % k
+            wheres.append(s)
+        if len(wheres) != 0:
+            wheres = " AND ".join(wheres)
+
+        print wheres
+        args = where_dict.values()
+
+        if len(wheres)==0:
+            sql = 'SELECT %s FROM %s ' % (selects, "%s %s %s" % (table['name'], table['alias'], joins))
+        else:
+            sql = 'SELECT %s FROM %s WHERE %s' % (
+                selects,
+                "%s %s %s" % (table['name'], table['alias'], joins),
+                wheres
+            )
+
+        if group_by_fields is not None:
+            group_bys = ','.join(group_by_fields)
+            sql += ' GROUP BY %s' % group_bys
+
+        if order_by_fields is not None:
+            order_bys = ','.join(order_by_fields)
+            sql += ' ORDER BY %s' % order_bys
+
+        if limit_conf is not None:
+            # select_type = "list"
+            sql += ' LIMIT %s, %s' % (limit_conf['start'], limit_conf['count'])
+
+        lock_str = " FOR UPDATE " if lock == True else ""
 
         if select_type=="get":
-            return self.get(sql+" LIMIT 1", *args)
+            print sql+" LIMIT 1"+lock_str, args
+            return self.get(sql+" LIMIT 1"+lock_str, *args)
         else:
-            return self.query(sql, *args)
+            print sql+lock_str, args
+            return self.query(sql+lock_str, *args)
+
+
+    update_process = {
+        "__dec_": lambda k, updates: ('%s=%s-%%s' % (k, k), updates[k]['__dec_']),
+        "__inc_": lambda k, updates: ('%s=%s+%%s' % (k, k), updates[k]['__inc_']),
+        "__eq_": lambda k, updates: ('%s=%s' % (k, updates[k]['__eq_']), None)
+    }
+
+    update_where_process = {
+        "__in_": lambda k, where_dict: '%s in (%s)' % ( k, where_dict[k]['__in_'] ),
+        # "__ne_": lambda k, where_dict: '%s!=%s' % (k, where_dict[k]['__ne_']),
+        # "__lt_": lambda k, where_dict: '%s<%s' % (k, where_dict[k]['__lt_']),
+        # "__lte_": lambda k, where_dict: '%s<=%s' % (k, where_dict[k]['__lte_']),
+        # "__gt_": lambda k, where_dict: '%s>%s' % (k, where_dict[k]['__gt_']),
+        # "__gte_": lambda k, where_dict: '%s>=%s' % (k, where_dict[k]['__gte_']),
+        # "__like_": lambda k, where_dict: "%s LIKE '%s%%%%'" % (k, where_dict[k]['__like_']),
+        # "__all_like_": lambda k, where_dict: "%s LIKE '%%%%%s%%%%'" % (k, where_dict[k]['__all_like_']),
+    }
+
+
+    def update_table_by_wheres(self, table_name, updates, where_dict):
+        '''
+            根据条件更新记录 add by ghostbod
+            {"current_quantity": {'__inc_':ibp_item['quantity_actual']} }
+            {"current_quantity": {'__eq_':'quantity_actual'} }
+
+        '''
+        sets = []
+        for k in updates.keys():
+            if type(updates[k])==dict:
+                protype = updates[k].keys()[0]
+                if protype in self.update_process.keys():
+                    s, updates[k] = self.update_process[protype](k, updates)
+                    if updates[k] == None:
+                        updates.pop(k)
+            else:
+                s = '%s=%%s' % k
+            sets.append(s)
+        sets = ','.join(sets)
+
+        wheres = []
+        for k in where_dict.keys():
+            if type(where_dict[k])==dict:
+                protype = where_dict[k].keys()[0]
+                if protype in self.select_process.keys():
+                    s = self.select_process[protype](k, where_dict)
+                where_dict.pop(k)
+            else:
+                s = '%s=%%s' % k
+            wheres.append(s)
+        wheres = " AND ".join(wheres)
+
+        sql = 'UPDATE %s SET %s WHERE %s' % (
+            table_name,
+            sets,
+            wheres
+        )
+        args = updates.values()
+
+        args += where_dict.values()
+
+        print sql, args
+        return self.execute(sql, *args)
 
 
 
-
-
-    def update_table_by_fields(self, table_name, updates,
-                     where_fields, where_values):
+    def update_table_by_fields(self, table_name, updates, where_fields, where_values):
+        '''根据条件更新记录'''
         sets = []
         for k in updates.keys():
             s = '%s=%%s' % k
@@ -269,9 +473,26 @@ class Connection(object):
         print sql, args
         return self.execute(sql, *args)
 
+    def delete_table_by_wheres(self, table_name, where_dict):
+        '''根据字典条件删除记录'''
+        wheres = []
+        for k in where_dict.keys():
+            s = '%s=%%s' % k
+            wheres.append(s)
+        wheres = " AND ".join(wheres)
+
+        sql = 'DELETE FROM %s WHERE %s' % (
+            table_name,
+            wheres
+        )
+        args = where_dict.values()
+        print sql, args
+        return self.execute(sql, *args)
+
 
     def delete_table_by_fields(self, table_name,
                      where_fields, where_values):
+        '''根据条件删除记录'''
         wheres = []
         for k in where_fields:
             s = '%s=%%s' % k
@@ -283,8 +504,7 @@ class Connection(object):
             wheres
         )
         args = where_values
-        print sql
-        print args
+        print sql, args
         return self.execute(sql, *args)
 
 
@@ -298,9 +518,10 @@ class Connection(object):
             r = self.execute(sql, *(item.values()))
             return r
         except Exception, e:
+            print '--------DB Exception-------\n', e
             #traceback.print_exc()
             if e[0] == 1062: # just skip duplicated item
-                pass
+                return -1062, 0
             else:
                 # print 'item:'
                 # for k,v in item.items():
